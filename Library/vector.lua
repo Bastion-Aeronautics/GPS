a02d104549204711ba2d4a0df38c5f4d88b55ef8
Savroo <savro1248@gmail.com>
2026-04-30 00:57:49 +1000

    fixed the library

--- Library/vector.lua
+++ Library/vector.lua
@@ -2,11 +2,9 @@ vec=function(x,y,z) return {x=x,y=y,z=z} end
 function vec_add(A,B) return vec(A.x+B.x,A.y+B.y,A.z+B.z) end
 function vec_sub(A,B) return vec(A.x-B.x,A.y-B.y,A.z-B.z) end
 function vec_scal(A,n) return vec(A.x*n,A.y*n,A.z*n) end
-function vec_neg(A) return vec(-A.x,-A.y,-A.z) end
-function vec_mult(A,n) return vec(A.x*n,A.y*n,A.z*n) end
 function vec_dot(A,B) return A.x*B.x+A.y*B.y+A.z*B.z end
-function vec_div(A,n) return vec(A.x/n,A.y/n,A.z/n) end
+function vec_div(A,n) return vec_scal(A,1/n) end
 function vec_length(A) return math.sqrt(A.x*A.x+A.y*A.y+A.z*A.z) end
-function vec_norm(A) return A.x~=0 and vec_div(A,vec_length(A)) or vec(0,0,0) end
+function vec_norm(A) return vec_length(A)~=0 and vec_div(A,vec_length(A)) or vec(0,0,0) end
 function vec_cross(A,B) return vec(A.y*B.z-A.z*B.y,A.z*B.x-A.x*B.z,A.x*B.y-A.y*B.x) end
-function vec_lerp(A,B,t) return vec((A.x+(B.x-A.x)*t),(A.y+(B.y-A.y)*t),(A.z+(B.z-A.z)*t)) end
+function vec_lerp(A,B,t) return vec_add(A,vec_scal(vec_sub(B,A),t)) end
