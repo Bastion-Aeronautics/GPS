07af13563f2936ed0b992177418d80b359aec067
Savroo <savro1248@gmail.com>
2026-05-12 12:43:54 +1000

    Create math.lua

---
+++ .Library/math.lua
@@ -0,0 +1,5 @@
+pi=math.pi
+tau=math.pi*2
+function clamp(val, min, max) return math.min(math.max(val, min), max) end
+function lerp(a, b, t) return a + (b - a) * t end
+function map(t, a, b, c, d) return ((t - a) / (b - a)) * (d - c) + c end -- map a-b to c-d according to t
